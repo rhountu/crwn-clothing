1ee22fa107be81af3490a27f73028746d20d521b
rhountu <rahirawat95@gmail.com>
2020-05-07 13:28:18 +0530

    Added the  header with logo and shop,contact link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route ,Switch} from 'react-router-dom';
 import { Homepage } from './pages/homepage/homepage.component'
 import './App.css';
 import ShopPage  from './pages/shop/shop.component'
+import  Header from  './components/header/header.component.jsx'
 
 
 const Hatspage=(props)=>{
@@ -17,6 +18,7 @@ const Hatspage=(props)=>{
 function App() {
   return (
     <div >
+      <Header to='/'/>
      <Switch>
        <Route exact path='/' component={Homepage}/>
        <Route  path='/hats' component={Hatspage}/>
