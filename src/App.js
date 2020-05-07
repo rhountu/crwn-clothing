0519c23e575b44a3e4d3bd564f1a55997bbc025f
rhountu <rahirawat95@gmail.com>
2020-05-07 12:40:12 +0530

    Updating app with shop page, collection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Route ,Switch} from 'react-router-dom';
- import { Homepage } from './pages/homepage/homepage.component'
+import { Homepage } from './pages/homepage/homepage.component'
 import './App.css';
+import ShopPage  from './pages/shop/shop.component'
 
 
 const Hatspage=(props)=>{
@@ -19,6 +20,7 @@ function App() {
      <Switch>
        <Route exact path='/' component={Homepage}/>
        <Route  path='/hats' component={Hatspage}/>
+       <Route path='/shop' component={ShopPage}/>
        <Route/>
      </Switch>
     </div>
